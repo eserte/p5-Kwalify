95660199a0ba596b968e30cc6921731aaeefdecf
eserte <eserte>
2006-11-18 00:11:51 +0000

    Initial revision

---
+++ lib/Kwalify.pm
@@ -0,0 +1,405 @@
+# -*- perl -*-
+
+#
+# $Id: Kwalify.pm,v 1.1 2006/11/18 00:13:17 eserte Exp $
+# Author: Slaven Rezic
+#
+# Copyright (C) 2006 Slaven Rezic. All rights reserved.
+# This package is free software; you can redistribute it and/or
+# modify it under the same terms as Perl itself.
+#
+# Mail: slaven@rezic.de
+# WWW:  http://www.rezic.de/eserte/
+#
+
+package Kwalify;
+
+use strict;
+
+use base qw(Exporter);
+use vars qw(@EXPORT_OK $VERSION);
+@EXPORT_OK = qw(validate);
+
+$VERSION = sprintf("%d.%02d", q$Revision: 1.1 $ =~ /(\d+)\.(\d+)/);
+
+sub validate ($$) {
+    my($schema, $data) = @_;
+    my $self = Kwalify->new;
+    $self->_validate($schema, $data, "/", []);
+    if (@{$self->{errors}}) {
+	die join("\n", @{$self->{errors}}) . "\n";
+    } else {
+	1;
+    }
+}
+
+sub new {
+    my($class) = @_;
+    bless { errors => [] }, $class;
+}
+
+sub _validate {
+    my($self, $schema, $data, $path, $prev_context, $args) = @_;
+    $self->{path} = $path;
+
+    if (!UNIVERSAL::isa($schema, "HASH")) {
+	$self->_die("Schema structure must be a hash reference");
+    }
+
+    my @context = (@$prev_context, "");
+    $self->{context} = \@context;
+
+    my $type = $schema->{type};
+    if (!defined $type) {
+	$type = 'str'; # default type;
+	$context[-1] .= "using default type `str'\n";
+    }
+    my $type_check_method = "_validate_" . $type;
+    if (!$self->can($type_check_method)) {
+	$self->_die("Invalid or unimplemented type `$type'");
+    }
+
+    $self->$type_check_method($schema, $data, $path, \@context, $args);
+}
+
+sub _additional_rules {
+    my($self, $schema, $data, $path) = @_;
+    if (defined $schema->{pattern}) {
+	(my $pattern = $schema->{pattern}) =~ s{^/(.*)/$}{$1};
+	if ($data !~ qr{$pattern}) {
+	    $self->_error("Non-valid data `$data' does not match /$pattern/");
+	}
+    }
+    if (defined $schema->{length}) {
+	if (!UNIVERSAL::isa($schema->{length}, "HASH")) {
+	    $self->_die("`length' must be a hash with keys max and/or min");
+	}
+	my $length = length($data);
+	if (exists $schema->{length}->{min}) {
+	    my $min = $schema->{length}->{min};
+	    if ($length < $min) {
+		$self->_error("`$data' is too short (length $length < min $min)");
+	    }
+	}
+	if (exists $schema->{length}->{'min-ex'}) {
+	    my $min = $schema->{length}->{'min-ex'};
+	    if ($length <= $min) {
+		$self->_error("`$data' is too short (length $length <= min $min)");
+	    }
+	}
+	if (exists $schema->{length}->{max}) {
+	    my $max = $schema->{length}->{max};
+	    if ($length > $max) {
+		$self->_error("`$data' is too long (length $length > max $max)");
+	    }
+	}
+	if (exists $schema->{length}->{'max-ex'}) {
+	    my $max = $schema->{length}->{'max-ex'};
+	    if ($length > $max) {
+		$self->_error("`$data' is too long (length $length >= max $max)");
+	    }
+	}
+    }
+    if (defined $schema->{enum}) {
+	if (!UNIVERSAL::isa($schema->{enum}, 'ARRAY')) {
+	    $self->_die("`enum' must be an array");
+	}
+	my %valid = map { ($_,1) } @{ $schema->{enum} };
+	if (!exists $valid{$data}) {
+	    $self->_error("`$data': invalid " . _base_path($path) . " value");
+	}
+    }
+    if (defined $schema->{range}) {
+	if (!UNIVERSAL::isa($schema->{range}, "HASH")) {
+	    $self->_die("`range' must be a hash with keys max and/or min");
+	}
+ 	my($lt, $le, $gt, $ge);
+## yes? no?
+# 	if (eval { require Scalar::Util; defined &Scalar::Util::looks_like_number }) {
+# 	    if (Scalar::Util::looks_like_number($data)) {
+# 		$lt = sub { $_[0] < $_[1] };
+# 		$gt = sub { $_[0] > $_[1] };
+# 	    } else {
+# 		$lt = sub { $_[0] lt $_[1] };
+# 		$gt = sub { $_[0] gt $_[1] };
+# 	    }
+# 	} else {
+#	    warn "Cannot determine whether $data is a number, assume so..."; # XXX show only once
+	    no warnings 'numeric';
+	    $lt = sub { $_[0] < $_[1] };
+	    $gt = sub { $_[0] > $_[1] };
+	    $le = sub { $_[0] <= $_[1] };
+	    $ge = sub { $_[0] >= $_[1] };
+#	}
+	    
+	if (exists $schema->{range}->{min}) {
+	    my $min = $schema->{range}->{min};
+	    if ($lt->($data, $min)) {
+		$self->_error("`$data' is too small (< min $min)");
+	    }
+	}
+	if (exists $schema->{range}->{'min-ex'}) {
+	    my $min = $schema->{range}->{'min-ex'};
+	    if ($le->($data, $min)) {
+		$self->_error("`$data' is too small (<= min $min)");
+	    }
+	}
+	if (exists $schema->{range}->{max}) {
+	    my $max = $schema->{range}->{max};
+	    if ($gt->($data, $max)) {
+		$self->_error("`$data' is too large (> max $max)");
+	    }
+	}
+	if (exists $schema->{range}->{'max-ex'}) {
+	    my $max = $schema->{range}->{'max-ex'};
+	    if ($ge->($data, $max)) {
+		$self->_error("`$data' is too large (>= max $max)");
+	    }
+	}
+    }
+    if (defined $schema->{assert}) {
+	$self->_die("`assert' is not yet implemented");
+    }
+}
+
+sub _validate_text {
+    my($self, $schema, $data, $path) = @_;
+    if (!defined $data || ref $data) {
+	return $self->_error("Non-valid data `" . (defined $data ? $data : 'undef') . "', expected text");
+    }
+    $self->_additional_rules($schema, $data, $path);
+}
+
+sub _validate_str {
+    my($self, $schema, $data, $path) = @_;
+    if (!defined $data || ref $data || $data =~ m{^\d+(\.\d+)?$}) {
+	return $self->_error("Non-valid data `" . (defined $data ? $data : 'undef') . "', expected a str");
+    }
+    $self->_additional_rules($schema, $data, $path);
+}
+
+sub _validate_int {
+    my($self, $schema, $data, $path) = @_;
+    if ($data !~ m{^[+-]?\d+$}) { # XXX what about scientific notation?
+	$self->_error("Non-valid data `" . $data . "', expected an int");
+    }
+    $self->_additional_rules($schema, $data, $path);
+}
+
+sub _validate_float {
+    my($self, $schema, $data, $path) = @_;
+    if ($data !~ m{^[+-]?\d+\.\d+$}) { # XXX other values?
+	$self->_error("Non-valid data `" . $data . "', expected a float");
+    }
+    $self->_additional_rules($schema, $data, $path);
+}
+
+sub _validate_number {
+    my($self, $schema, $data, $path) = @_;
+    if ($data !~ m{^[+-]?\d+(\.\d+)?$}) { # XXX combine int+float regexp!
+	$self->_error("Non-valid data `" . $data . "', expected a number");
+    }
+    $self->_additional_rules($schema, $data, $path);
+}
+
+sub _validate_bool {
+    my($self, $schema, $data, $path) = @_;
+    if ($data !~ m{^(yes|true|1|no|false|0)$}) { # XXX correct?
+	$self->_error("Non-valid data `" . $data . "', expected a boolean");
+    }
+    $self->_additional_rules($schema, $data, $path);
+}
+
+sub _validate_date {
+    my($self, $schema, $data, $path) = @_;
+    if ($data !~ m{^\d{4}-\d{2}-\d{2}$}) {
+	$self->_error("Non-valid data `" . $data . "', expected a date (YYYY-MM-DD)");
+    }
+    $self->_additional_rules($schema, $data, $path);
+}
+
+sub _validate_time {
+    my($self, $schema, $data, $path) = @_;
+    if ($data !~ m{^\d{2}:\d{2}:\d{2}$}) {
+	$self->_error("Non-valid data `" . $data . "', expected a time (HH:MM:SS)");
+    }
+    $self->_additional_rules($schema, $data, $path);
+}
+
+sub _validate_timestamp {
+    my($self) = @_;
+    $self->_error("timestamp validation NYI"); # XXX
+}
+
+sub _validate_any {
+    1;
+}
+
+sub _validate_seq {
+    my($self, $schema, $data, $path, $prev_context) = @_;
+    my @context = (@$prev_context, "");
+    $self->{context} = \@context;
+    if (!exists $schema->{sequence}) {
+	$self->_die("`sequence' missing with `seq' type");
+    }
+    my $sequence = $schema->{sequence};
+    if (!UNIVERSAL::isa($sequence, 'ARRAY')) {
+	$self->_die("Expected array in `sequence'");
+    }
+    if (@$sequence != 1) {
+	$self->_die("Expect exactly one element in sequence");
+    }
+    if (!UNIVERSAL::isa($data, 'ARRAY')) {
+	$self->_error("Non-valid data " . $data . ", expected sequence");
+    }
+    my $subschema = $sequence->[0];
+    my $unique = _get_boolean($subschema->{unique});
+    my %unique_val;
+    my %unique_mapping_val;
+    my $index = 0;
+    for my $elem (@$data) {
+	my $subpath = _append_path($path, $index);
+	$self->_validate($subschema, $elem, $subpath, \@context, { unique_mapping_val => \%unique_mapping_val});
+	if ($unique) {
+	    if (exists $unique_val{$elem}) {
+		$self->_error("`$elem' is already used at `$unique_val{$elem}'");
+	    } else {
+		$unique_val{$elem} = $subpath;
+	    }
+	}
+	$index++;
+    }
+}
+
+sub _validate_map {
+    my($self, $schema, $data, $path, $prev_context, $args) = @_;
+    my @context = (@$prev_context, "");
+    $self->{context} = \@context;
+    my $unique_mapping_val;
+    if ($args && $args->{unique_mapping_val}) {
+	$unique_mapping_val = $args->{unique_mapping_val};
+    }
+    if (!exists $schema->{mapping}) {
+	$self->_die("`mapping' missing with `map' type");
+    }
+    my $mapping = $schema->{mapping};
+    if (!UNIVERSAL::isa($mapping, 'HASH')) {
+	$self->_die("Expected hash in `mapping'");
+    }
+    if (!UNIVERSAL::isa($data, 'HASH')) {
+	$self->_error("Non-valid data " . $data . ", expected mapping");
+    }
+    my %seen_key;
+    while(my($key,$subschema) = each %$mapping) {
+	my $subpath = _append_path($path, $key);
+	$self->{path} = $subpath;
+	if (!UNIVERSAL::isa($subschema, 'HASH')) {
+	    $self->_die("Expected subschema (a hash)");
+	}
+	my $required = _get_boolean($subschema->{required});
+	if (!exists $data->{$key}) {
+	    if ($required) {
+		$self->{path} = $path;
+		$self->_error("Expected required key `$key'");
+		next;
+	    } else {
+		next;
+	    }
+	}
+	my $unique = _get_boolean($subschema->{unique});
+	if ($unique) {
+	    if (defined $unique_mapping_val->{$key}->{val} && $unique_mapping_val->{$key}->{val} eq $data->{$key}) {
+		$self->_error("`$data->{$key}' is already used at `$unique_mapping_val->{$key}->{path}'");
+	    } else {
+		$unique_mapping_val->{$key} = { val  => $data->{$key},
+						path => $subpath,
+					      };
+	    }
+	}
+
+	$self->_validate($subschema, $data->{$key}, $subpath, \@context); # XXX context not correct...
+	$seen_key{$key}++;
+    }
+
+    while(my($key,$val) = each %$data) {
+	my $subpath = _append_path($path, $key);
+	$self->{path} = $subpath;
+	if (!$seen_key{$key}) {
+	    $self->_error("Unexpected key `$key'");
+	}
+    }
+}
+
+sub _die {
+    my($self, $msg) = @_;
+    $msg = "[$self->{path}] $msg"; #XXX . ", additional context information: " . "@{ $self->{context} }";
+    die $msg."\n";
+}
+
+sub _error {
+    my($self, $msg) = @_;
+    $msg = "[$self->{path}] $msg"; #XXX . ", additional context information: " . "@{ $self->{context} }";
+    push @{$self->{errors}}, $msg;
+    0;
+}
+
+# Functions:
+sub _append_path {
+    my($root, $leaf) = @_;
+    $root . ($root !~ m{/$} ? "/" : "") . $leaf;
+}
+
+sub _base_path {
+    my($path) = @_;
+    my($base) = $path =~ m{([^/]+)$};
+    $base;
+}
+
+sub _get_boolean {
+    my($val) = @_;
+    defined $val && $val =~ m{^(yes|true|1)$}; # XXX check for all boolean trues
+}
+
+1;
+__END__
+
+=head1 NAME
+
+Kwalify - schema for data structures
+
+=head1 SYNOPSIS
+
+  use Kwalify qw(validate);
+  validate($schema, $data);
+
+Typically used together with YAML or JSON:
+
+  use YAML;
+  validate(YAML::LoadFile($schema_file), YAML::LoadFile($data_file));
+
+  use JSON;
+  validate(...);
+
+=head1 DESCRIPTION
+
+
+=head2 EXPORT
+
+validate.
+
+=head1 SEE ALSO
+
+
+=head1 AUTHOR
+
+Slaven Rezic, E<lt>srezic@cpan.orgE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2006 by Slaven Rezic
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.8.8 or,
+at your option, any later version of Perl 5 you may have available.
+
+=cut
---
+++ t/Kwalify.t
@@ -0,0 +1,336 @@
+#!/usr/bin/perl -w
+# -*- perl -*-
+
+#
+# $Id: Kwalify.t,v 1.1 2006/11/18 00:13:38 eserte Exp $
+# Author: Slaven Rezic
+#
+
+use strict;
+
+BEGIN {
+    if (!eval q{
+	use Test::More;
+	1;
+    }) {
+	print "1..0 # skip: no Test::More module\n";
+	exit;
+    }
+}
+
+BEGIN {
+    plan tests => 35;
+}
+
+BEGIN {
+    use_ok('Kwalify', 'validate');
+}
+
+sub is_valid_yaml {
+    my($schema, $document, $testname) = @_;
+    local $Test::Builder::Level = $Test::Builder::Level+1;
+    ok(validate(YAML::Syck::Load($schema), YAML::Syck::Load($document)), $testname);
+}
+
+sub is_invalid_yaml {
+    my($schema, $document, $errors, $testname) = @_;
+    local $Test::Builder::Level = $Test::Builder::Level+1;
+    ok(!eval { validate(YAML::Syck::Load($schema), YAML::Syck::Load($document)) }, $testname);
+    for my $error (@$errors) {
+	if (UNIVERSAL::isa($error, 'HASH')) {
+	    my($pattern, $testname) = @{$error}{qw(pattern testname)};
+	    like($@, $pattern, $testname);
+	} else {
+	    like($@, $error);
+	}
+    }
+}
+
+SKIP: {
+    skip("Need YAML::Syck for tests", 1)
+	if !eval { require YAML::Syck; 1 };
+
+    my $schema01 = <<'EOF';
+type:   seq
+sequence:
+  - type:   str
+EOF
+    my $document01a = <<'EOF';
+- foo
+- bar
+- baz
+EOF
+    is_valid_yaml($schema01, $document01a, "sequence of str");
+
+    my $schema01b = <<'EOF';
+type:   seq
+sequence: [{}]
+EOF
+    is_valid_yaml($schema01b, $document01a, "sequence with default type (str)");
+
+    my $document01b = <<'EOF';
+- foo
+- 123
+- baz
+EOF
+
+    is_invalid_yaml($schema01,$document01b, 
+		    [qr{\Q[/1] Non-valid data `123', expected a str}],
+		    "Non valid data, int in sequence of str");
+    
+    my $schema02 = <<'EOF';
+type:       map
+mapping:
+  name:
+    type:      str
+    required:  yes
+  email:
+    type:      str
+    pattern:   /@/
+  age:
+    type:      int
+  birth:
+    type:      date
+EOF
+
+    my $document02a = <<'EOF';
+name:   foo
+email:  foo@mail.com
+age:    20
+birth:  1985-01-01
+EOF
+    is_valid_yaml($schema02, $document02a, "mapping");
+
+    my $document02b = <<'EOF';
+name:   foo
+email:  foo(at)mail.com
+age:    twenty
+birth:  Jun 01, 1985
+EOF
+    is_invalid_yaml($schema02, $document02b,
+		    [qr{\Q[/birth] Non-valid data `Jun 01, 1985', expected a date (YYYY-MM-DD)},
+		     qr{\Q[/age] Non-valid data `twenty', expected an int},
+		     qr{\Q[/email] Non-valid data `foo(at)mail.com' does not match /@/},
+		    ],
+		    "invalid mapping");
+
+    my $schema03 = <<'EOF';
+type:      seq
+sequence:
+  - type:      map
+    mapping:
+      name:
+        type:      str
+        required:  true
+      email:
+        type:      str
+EOF
+    my $document03a = <<'EOF';
+- name:   foo
+  email:  foo@mail.com
+- name:   bar
+  email:  bar@mail.net
+- name:   baz
+  email:  baz@mail.org
+EOF
+    is_valid_yaml($schema03, $document03a, "sequence of mapping");
+    my $document03b = <<'EOF';
+- name:   foo
+  email:  foo@mail.com
+- naem:   bar
+  email:  bar@mail.net
+- name:   baz
+  mail:   baz@mail.org
+EOF
+    is_invalid_yaml($schema03, $document03b,
+		    [qr{\Q[/1] Expected required key `name'},
+		     qr{\Q[/1/naem] Unexpected key `naem'},
+		     qr{\Q[/2/mail] Unexpected key `mail'},
+		    ]);
+
+    my $schema04 = <<'EOF';
+type:      map
+mapping:
+  company:
+    type:      str
+    required:  yes
+  email:
+    type:      str
+  employees:
+    type:      seq
+    sequence:
+      - type:    map
+        mapping:
+          code:
+            type:      int
+            required:  yes
+          name:
+            type:      str
+            required:  yes
+          email:
+            type:      str
+EOF
+    my $document04a = <<'EOF';
+company:    Kuwata lab.
+email:      webmaster@kuwata-lab.com
+employees:
+  - code:   101
+    name:   foo
+    email:  foo@kuwata-lab.com
+  - code:   102
+    name:   bar
+    email:  bar@kuwata-lab.com
+EOF
+    is_valid_yaml($schema04, $document04a, "mapping of sequence");
+    my $document04b = <<'EOF';
+company:    Kuwata Lab.
+email:      webmaster@kuwata-lab.com
+employees:
+  - code:   A101
+    name:   foo
+    email:  foo@kuwata-lab.com
+  - code:   102
+    name:   bar
+    mail:   bar@kuwata-lab.com
+EOF
+    is_invalid_yaml($schema04, $document04b,
+		    [qr{\Q[/employees/0/code] Non-valid data `A101', expected an int},
+		     qr{\Q[/employees/1/mail] Unexpected key `mail'},
+		    ]);
+
+    my $schema05 = <<'EOF';
+type:      seq                                # new rule
+sequence:
+  -
+    type:      map                            # new rule
+    mapping:
+      name:
+        type:       str                       # new rule
+        required:   yes
+      email:
+        type:       str                       # new rule
+        required:   yes
+        pattern:    /@/
+      password:
+        type:       text                      # new rule
+        length:     { max: 16, min: 8 }
+      age:
+        type:       int                       # new rule
+        range:      { max: 30, min: 18 }
+        # or assert: 18 <= val && val <= 30
+      blood:
+        type:       str                       # new rule
+        enum:
+          - A
+          - B
+          - O
+          - AB
+      birth:
+        type:       date                      # new rule
+      memo:
+        type:       any                       # new rule
+EOF
+    my $document05a = <<'EOF';
+- name:     foo
+  email:    foo@mail.com
+  password: xxx123456
+  age:      20
+  blood:    A
+  birth:    1985-01-01
+- name:     bar
+  email:    bar@mail.net
+  age:      25
+  blood:    AB
+  birth:    1980-01-01
+EOF
+    is_valid_yaml($schema05, $document05a, "Many rules");
+    my $document05b = <<'EOF';
+- name:     foo
+  email:    foo(at)mail.com
+  password: xxx123
+  age:      twenty
+  blood:    a
+  birth:    1985-01-01
+- given-name:  bar
+  family-name: Bar
+  email:    bar@mail.net
+  age:      15
+  blood:    AB
+  birth:    1980/01/01
+EOF
+    is_invalid_yaml($schema05, $document05b,
+		    [
+		     qr{\Q[/0/blood] `a': invalid blood value},
+		     qr{\Q[/0/email] Non-valid data `foo(at)mail.com' does not match /@/},
+		     qr{\Q[/0/password] `xxx123' is too short (length 6 < min 8)},
+		     qr{\Q[/0/age] Non-valid data `twenty', expected an int},
+		     qr{\Q[/0/age] `twenty' is too small (< min 18)},
+		     qr{\Q[/1/birth] Non-valid data `1980/01/01', expected a date (YYYY-MM-DD)},
+		     qr{\Q[/1] Expected required key `name'},
+		     qr{\Q[/1/age] `15' is too small (< min 18)},
+		     qr{\Q[/1/given-name] Unexpected key `given-name'},
+		     qr{\Q[/1/family-name] Unexpected key `family-name'},
+		    ]);
+
+    my $schema06 = <<'EOF';
+type: seq
+sequence:
+  - type:     map
+    required: yes
+    mapping:
+      name:
+        type:     str
+        required: yes
+        unique:   yes
+      email:
+        type:     str
+      groups:
+        type:     seq
+        sequence:
+          - type: str
+            unique:   yes
+EOF
+    my $document06a = <<'EOF';
+- name:   foo
+  email:  admin@mail.com
+  groups:
+    - users
+    - foo
+    - admin
+- name:   bar
+  email:  admin@mail.com
+  groups:
+    - users
+    - admin
+- name:   baz
+  email:  baz@mail.com
+  groups:
+    - users
+EOF
+    is_valid_yaml($schema06, $document06a, "unique");
+    my $document06b = <<'EOF';
+- name:   foo
+  email:  admin@mail.com
+  groups:
+    - foo
+    - users
+    - admin
+    - foo
+- name:   bar
+  email:  admin@mail.com
+  groups:
+    - admin
+    - users
+- name:   bar
+  email:  baz@mail.com
+  groups:
+    - users
+EOF
+    is_invalid_yaml($schema06, $document06b,
+		    [qr{\Q[/0/groups/3] `foo' is already used at `/0/groups/0'},
+		     qr{\Q[/2/name] `bar' is already used at `/1/name'},
+		    ]);
+
+}
+
+__END__
