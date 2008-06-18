73e08b0f8dd1a21e16550b2f77b3d42342c9bb13
eserte <eserte>
2008-06-18 21:40:38 +0000

    * version inc'd * copyright year

--- lib/Kwalify.pm
+++ lib/Kwalify.pm
@@ -1,10 +1,10 @@
 # -*- mode: cperl; coding: latin-2 -*-
 
 #
-# $Id: Kwalify.pm,v 1.20 2008/06/18 21:39:50 eserte Exp $
+# $Id: Kwalify.pm,v 1.21 2008/06/18 21:40:38 eserte Exp $
 # Author: Slaven Rezic
 #
-# Copyright (C) 2006,2007 Slaven Rezic. All rights reserved.
+# Copyright (C) 2006,2007,2008 Slaven Rezic. All rights reserved.
 # This package is free software; you can redistribute it and/or
 # modify it under the same terms as Perl itself.
 #
@@ -20,8 +20,8 @@ use base qw(Exporter);
 use vars qw(@EXPORT_OK $VERSION);
 @EXPORT_OK = qw(validate);
 
-$VERSION = '1.16';
-# sprintf("%d.%02d", q$Revision: 1.20 $ =~ /(\d+)\.(\d+)/);
+$VERSION = '1.17';
+# sprintf("%d.%02d", q$Revision: 1.21 $ =~ /(\d+)\.(\d+)/);
 
 BEGIN {
     if ($] < 5.006) {
@@ -451,7 +451,7 @@ Slaven Reziæ, E<lt>srezic@cpan.orgE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2006 by Slaven Reziæ
+Copyright (C) 2006,2007,2008 by Slaven Reziæ
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself, either Perl version 5.8.8 or,
